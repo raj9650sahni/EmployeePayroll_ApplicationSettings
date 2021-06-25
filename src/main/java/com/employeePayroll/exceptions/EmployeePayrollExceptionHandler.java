1fce6f1108e16b4aec4303819f91cb1eeae72704
Raj Kishore Sahni <rajkishoresahni826@gmail.com>
2021-06-25 22:58:16 +0530

    Added exception handler to throw exceptions

---
+++ src/main/java/com/employeePayroll/exceptions/EmployeePayrollExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.employeePayroll.exceptions;
+
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.employeePayroll.DTO.ResponseDTO;
+
+@ControllerAdvice
+public class EmployeePayrollExceptionHandler {
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException( MethodArgumentNotValidException exception) {
+		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+		java.util.List<String> errMesg=errorList.stream()
+										.map(objErr -> objErr.getDefaultMessage())
+										.collect(Collectors.toList());
+		
+		ResponseDTO responseDTO = new ResponseDTO("Exception While processing Resyt Request", errMesg);
+		return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.BAD_REQUEST);
+	}
+	
+	
+
+}
